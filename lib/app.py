9f362e5d8ff2a829ba18e0b1e05983e4b40f0f07
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-05-01 17:55:47 -0400

    experimenting with app logic

--- lib/app.py
+++ lib/app.py
@@ -1,31 +1,64 @@
-'''
-Taken from state capitals lab, need to rework to interface with sql models
-replay = "y"
-print("Welcome to Flash Card memorization\n")
-while replay == "y":
-    for state in states:
-        question = state["name"]
-        solution = state["capital"]
-        print(f"Name the capital of {question}. Press type 'h' for hint\n")
-        answer = 'h'
-        while answer == 'h':
-            answer = input("Your answer: ")
-            if answer == solution:
-                print(f"Correct! Answer was {solution}")
-                state['correct'] = state['correct'] + 1
-                correct = correct+1
-                print(f"{correct} out of 50")
-                break    
-            elif answer == 'h':
-                print (f"{solution[0:3]}\n")
-            else:
-                print(f"Wrong! Answer was {solution}")
-                state['incorrect'] = state['incorrect'] + 1
-                print(f"{correct} out of 50")
-                break
-    replay = input("Play again?(y/n)")
-    if replay=='n':
-        print("exiting...")
+import argparse
+from peewee import *
+
+db = PostgresqlDatabase('flashcardapp', user='postgres', password='', host='localhost', port=5432)
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class FlashCard(BaseModel):
+    front_text  = CharField()
+    back_text = CharField()
+    times_correct = IntegerField()
+    times_incorrect = IntegerField()
+    
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+#This function will trigger an addition of a card to the database
+def create(args):
+    for (range(args.quantity)):
+        newfront_text = input("Please add cards front text")
+        newback_text = input("Please add cards back text")
+        db.create_tables([FlashCard])
+        newCard = FlashCard(front_text=newfront_text, back_text=newback_text,times_correct=0,times_incorrect=0)
+        newCard.save()
+        print("Card created")
+#This function will bring back a number of cards from the database to test the user
+def session(args):
+    print(args.quantity)
+def main(args):
+    choice = 0 
+    if args.start:
+        session(args)
+    elif args.create:
+        create()
     else:
-        states = sorted(states, key=lambda i: i['incorrect'],reverse=True)
-        '''
+        while choice != "3":
+            print("Welcome to the FlashCard app.\n")
+            choice = input("Please pick an option by typing its number.\n[1]CREATE new card\n[2]START new practice\n[3]QUIT\n")
+            if choice == "1":
+                create()
+            elif choice =="2":
+                session(args)
+            elif choice == "3":
+                print("exiting...")
+                break
+            else: 
+                print("error. please type a number when prompted.")
+
+main_parser = subparsers.add_parser('flash')
+main_parser.add_argument('quantity')
+main_parser.add_argument('--start', action='store_true')
+main_parser.add_argument('--create', action='store_true')
+main_parser.set_defaults(func=main)
+
+if __name__ == '__main__':
+    args = parser.parse_args() 
+    try:
+        func = args.func
+    except AttributeError:
+        parser.error("too few arguments")
+    func(args) # call the default function
